6fd4f58c82b785efa55a1717252ad9e781ed6746
Kirill Sidorkin <sidorkin.kirill.gm@gmail.com>
2017-12-20 00:13:30 +0200

    Homework 6

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-function mas(a){
-  var mas1=[];
-  var mas2=[];
-  for(var i=0; i<a.length-1; i++){
-    if(a[i+1]-a[i]<=0){
-      mas1.push(a[i]);
-    } 
-    if(i!==0 && a[i+1]-a[i-1]<0){
-      mas2.push(a[i]);
+function tick(a){
+  if (a>=10 && a<=Math.pow(10,6)){
+    var s=a.toString();
+    if (s.length%2 ===0){
+      var left=0;
+      var right=0;
+      for (var i=0;i<s.length/2;i++){
+        left+= Number(s[i]);
+      }
+      for (var j=s.length/2;j<s.length;j++){
+        right+= Number(s[j]);
+      }
+      if(left===right){
+        return true;
+      }
+      else{
+        return false;
+      }
     }
-  }
-  if (mas1.length<=1 && mas2.length<=1){
-    return true;
+    else{
+      return false;
+    }
   }
   else{
     return false;
   }
 }
-var a= [1, 1, 1, 2, 3];
-mas(a);
+tick(239017);
