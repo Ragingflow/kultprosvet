707e927f5da922e3771c8e61cd226f93beffa108
Kirill Sidorkin <sidorkin.kirill.gm@gmail.com>
2017-12-23 16:07:51 +0200

    Homework 7

--- js/script.js
+++ js/script.js
@@ -1,28 +1,72 @@
-function tick(a){
-  if (a>=10 && a<=Math.pow(10,6)){
-    var s=a.toString();
-    if (s.length%2 ===0){
-      var left=0;
-      var right=0;
-      for (var i=0;i<s.length/2;i++){
-        left+= Number(s[i]);
-      }
-      for (var j=s.length/2;j<s.length;j++){
-        right+= Number(s[j]);
-      }
-      if(left===right){
-        return true;
-      }
-      else{
-        return false;
-      }
+function mas(m){
+  var sum=0;
+  function length(){
+    if ( 1<=m.length && m.length<=5){
+      return true;
     }
     else{
       return false;
     }
   }
+  function stroke(){
+    for (var i=0;i<m.length;i++){
+      if (1<=m[i].length && m[i].length<=5){
+        sum++;
+      }
+      else{
+        sum--;
+      }
+    }
+    if (sum===m.length){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  function column(){
+    sum=0;
+    for (var i=0;i<m.length;i++){
+      for (var j=0;j<m[i].length;j++){
+        if (0<=m[i][j] && m[i][j]<=10){
+          sum++;
+        }
+        else{
+          sum--;
+        }
+      }
+      if (sum!==m[i].length){
+      return false;
+      }
+      else if (i===m.length-1){
+        return true;
+      }
+      else{
+        sum=0;
+        continue;
+      }
+    }
+    
+  }
+  if (length(m)==1 && stroke(m)==1 && column(m)==1){
+    sum=0;
+    for ( var i=0;i<m.length;i++){
+      for (var j=0;j<m[i].length;j++){
+        if (m[i][j]===0 && i<m.length-1 && m[i+1][j]!==undefined){
+          m[i+1][j]=0;
+        }
+        else{
+          sum+=m[i][j];
+        }
+      }
+    }
+  }
   else{
     return false;
   }
+  return sum;
 }
-tick(239017);
+var m = [[0, 1, 1, 2],
+        [0, 5, 0, 0,1],
+        [2, 0, 3]];
+mas(m)
