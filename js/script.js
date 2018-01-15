922422814407694756fbcc77da63a8358e1bfb70
Kirill Sidorkin <sidorkin.kirill.gm@gmail.com>
2018-01-15 18:54:32 +0200

    Homework 9

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-function mas(m){
-  var long=m[0].length;
-  var newmas=[];
-  for (var i=0;i<m.length;i++){
-    if (m[i].length>long){
-      long=m[i].length;
-    }
-    else{
-      continue;
+function same(a,b){
+  var sum=0;
+  var reg=/^[a-z]+$/;
+  if (!reg.test(a) || !reg.test(b)){
+    return false;
+  }
+  var x=a.split('');
+  var y=b.split('');
+  for (var i=0;i<x.length;i++){
+    for (var j=0;j<y.length;j++){
+      if (x[i]==y[j]){
+        delete y[j];
+        sum++;
+        break;
+      }
+      else continue;
     }
   }
-  for (i=0;i<m.length;i++){
-    if (m[i].length>=long){
-      newmas.push(m[i]);
-    }
-    else{
-      continue;
-    }
-  }
-  return newmas;
+  return sum;
 }
-var m=['aba', 'aa','ad' , 'vsd', 'aba'];
-mas(m);
+var a = "aabcc",b = "adcaa";
+same(a,b);
